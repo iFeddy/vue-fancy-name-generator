3d9242ced5f2e54bc7592a7df99b79e5aa52e2da
Federico Perez <feddy_7990@hotmail.com>
2021-07-15 12:48:32 -0300

    Added dist folder

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import Toaster from '@meforma/vue-toaster'
 import VTooltip from 'v-tooltip'
 import Router from "./router/index"
 import App from './App.vue'
+import VueGtag from "vue-gtag-next"
+
 import './index.css'
 
-createApp(App).use(VTooltip).use(Toaster).use(Router).mount('#app')
+const app = createApp(App);
+
+app.use(VTooltip);
+app.use(Toaster);
+app.use(Router);
+
+app.use(VueGtag, {
+  property: {
+    id: "GTM-WT94C3K"
+  }
+});
+
+
+app.mount("#app");
